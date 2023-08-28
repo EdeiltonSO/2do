c7e55581373346f5790cfe0b777e088d7e4fb9b7
Edeilton Oliveira <edeiltonso@gmail.com>
2023-08-27 23:11:37 -0300

    feat: add header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,20 @@
-import { Header } from "./styles";
+import logo2do from '../../assets/logo.svg'
+
+import { Button, FormContainer, Header, Task, TaskContainer, TaskCounter, TaskHeader, TaskInput, TaskList } from "./styles";
 
 export function Home() {
     return (
-        <Header>
-            
-        </Header>
+        <>
+            <Header>
+                <img src={logo2do} alt="" />
+
+                <FormContainer>
+                    <form onSubmit={() => {}} action="">
+                        <TaskInput placeholder='Adicione uma nova tarefa' type="text" />
+                        <Button>Criar</Button>
+                    </form>
+                </FormContainer>
+            </Header>
+        </>
     );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,4 +4,52 @@ export const Header = styled.header`
     width: 100%;
     height: 12.5rem;
     background-color: ${props => props.theme['gray700']};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 1.6875rem;
+
+    img {
+        width: 7.875rem;
+        height: 3rem;
+        margin-top: 4.5rem;
+    }
 `;
+
+export const FormContainer = styled.div`
+    width: 46rem;
+    height: 3.375rem;
+    // border: 1px solid #f00;
+    margin-top: 3.3125rem;
+    
+    form {
+        display: flex;
+        justify-content: space-between;
+        gap: 0.625rem;
+    }
+`;
+
+export const TaskInput = styled.input`
+    width: 100%;
+    height: 3.375rem;
+    border-radius: 8px;
+    padding: 1rem;
+    border: 1px solid ${props => props.theme.gray700};
+    background-color: ${props => props.theme.gray500};
+    color: ${props => props.theme.gray100};
+
+    &::placeholder {
+    color: ${props => props.theme.gray300};
+  }
+`;
+
+export const Button = styled.button`
+    height: 3.375rem;
+    border: none;
+    padding: 1rem;
+    background-color: ${props => props.theme.blueDark};
+    color: ${props => props.theme.gray100};
+    border-radius: 8px;
+    font-weight: bold;
+`;
+
